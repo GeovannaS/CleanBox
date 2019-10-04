f3921ae073015a40112d81a30a02a40f12743f5a
Geovanna <geoscabio@yahoo.com.br>
2019-10-03 22:22:12 -0300

    testing first commit from sourcetree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+//testing commit github
 export class AppComponent {
   title = 'CleanBox';
 }
